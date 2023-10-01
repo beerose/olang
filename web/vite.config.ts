9b593d58d4c95f99b8694a1f6a56edbb07c87d82
beerose <alexsandra.sikora@gmail.com>
2023-10-01 15:19:16 +0200

    add vercel plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import vercel from "vite-plugin-vercel"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
 })
