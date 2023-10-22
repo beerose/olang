c419ae1a35c06d189c0e8eb1720281de374e91ae
beerose <alexsandra.sikora@gmail.com>
2023-10-22 18:52:04 -0400

    add app type

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import vercel from "vite-plugin-vercel"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vercel()],
+  appType: 'spa',
 })
